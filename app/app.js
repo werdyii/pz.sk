162c18ab009cf83a54413aa2b43430b85900629d
Gusepe Werdy <gusepe.werdy@gmail.com>
2015-08-11 11:42:46 +0200

    Add bootstrap to bower

--- app/app.js
+++ app/app.js
@@ -1,12 +1,19 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
+angular.module('myAppPZ', [
   'ngRoute',
   'myApp.view1',
   'myApp.view2',
   'myApp.version'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider
+  	.when('/o-nas',{
+  		templateUrl:'o-nas.html',
+  		controller:'MessageController'
+  	})
+  	.otherwise({
+  		redirectTo: '/'
+  	});
 }]);
